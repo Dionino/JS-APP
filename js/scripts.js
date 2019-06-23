a1a379d482f14fe582388cdf7653a877a2bb5969
Dionino <51341014+Dionino@users.noreply.github.com>
2019-06-23 11:21:51 +0100

    functional programming principles added

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-var repository = [
+var pokemonRepository = (function () {
+  var repository = [
   {name: "Bulbasaur", height: 2.04, types: ["Grass", "Poison"]},
   {name: "Ivysaur", height: 3.03, types: ["Grass", "Poison"]},
   {name: "Venusaur", height: 6.07, types: ["Grass", "Poison"]},
@@ -7,13 +8,28 @@ var repository = [
   {name: "Charizard", height: 5.07, types: ["Fire", "Flying"]},
   {name: "Squirtle", height: 1.08, types: ["Water"]},
   {name: "Wartortle", height: 3.03, types: ["Water"]},
-  {name: "Blastoise", height: 5.03, types: ["Water"]}
-];
+  {name: "Blastoise", height: 5.03, types: ["Water"]}];
 
-repository.forEach(function(pokemonDetails){
-  if (pokemonDetails.height >= 6.07) {
-  document.write('name:' + ' ' + pokemonDetails.name + ', ' + '(height:' + ' ' + pokemonDetails.height + ') ' + '- Wow that\'s big!' + '<br>' + 'types:' + ' ' + pokemonDetails.types + '<br>' + '<br>')
+  function add(pokemon) {
+    return repository.push(pokemon);
+  }
+
+  function getAll() {
+      return repository;
+    }
+
+    return {
+      add: add,
+      getAll: getAll
+    };
+})();
+
+var pokemonDetails = pokemonRepository.getAll();
+
+pokemonDetails.forEach(function(allPokemon){
+  if (allPokemon.height >= 6.07) {
+  document.write('name:' + ' ' + allPokemon.name + ', ' + '(height:' + ' ' + allPokemon.height + ') ' + '- Wow that\'s big!' + '<br>' + 'types:' + ' ' + allPokemon.types + '<br>' + '<br>')
 }else {
-  document.write('name:' + ' ' + pokemonDetails.name + ', ' + '(height:' + ' ' + pokemonDetails.height + ') ' + '<br>' + 'types:' + ' ' + pokemonDetails.types + '<br>' + '<br>')
+  document.write('name:' + ' ' + allPokemon.name + ', ' + '(height:' + ' ' + allPokemon.height + ') ' + '<br>' + 'types:' + ' ' + allPokemon.types + '<br>' + '<br>')
 }
 });
